93e08b81fd04b460c13253d79301240f3be98946
Erik Hesselink <hesselink@gmail.com>
2024-12-04 10:20:26 +0100

    Format

--- src/day4.ts
+++ src/day4.ts
@@ -1,6 +1,6 @@
-import {readFile} from "fs/promises";
-import {sum} from "./array";
-import {Grid, Point, Direction, directions, move, getAt} from "./grid";
+import { readFile } from "fs/promises";
+import { sum } from "./array";
+import { Grid, Point, Direction, directions, move, getAt } from "./grid";
 
 const xmas = ["X", "M", "A", "S"] as const
 export type Char = typeof xmas[number]
@@ -14,13 +14,13 @@ async function readInput(): Promise<Grid<Char>> {
 
 export async function part1() {
   const grid = await readInput();
-  const total = sum(grid.map((l, y) => sum(l.map((_, x) => countXmasAt({x, y}, grid)))))
+  const total = sum(grid.map((l, y) => sum(l.map((_, x) => countXmasAt({ x, y }, grid)))))
   return total
 }
 
 export async function part2() {
   const grid = await readInput();
-  const total = sum(grid.map((l, y) => l.filter((_, x) => hasMasXAt({x, y}, grid)).length))
+  const total = sum(grid.map((l, y) => l.filter((_, x) => hasMasXAt({ x, y }, grid)).length))
   return total
 }
 
@@ -62,5 +62,6 @@ function hasMasXAt(p: Point, grid: Grid<Char>): boolean {
   const se = getAt(move(p, "SE"), grid);
   const sw = getAt(move(p, "SW"), grid);
   const ne = getAt(move(p, "NE"), grid);
-  return (nw === "M" && se === "S" || nw === "S" && se === "M") && (sw === "M" && ne === "S" || sw === "S" && ne === "M")
+  return (nw === "M" && se === "S" || nw === "S" && se === "M")
+    && (sw === "M" && ne === "S" || sw === "S" && ne === "M")
 }
--- src/grid.ts
+++ src/grid.ts
@@ -3,19 +3,19 @@ export type Point = {
   y: number;
 };
 
-export const directions =  ["N", "NW", "W", "SW", "S", "SE", "E", "NE"] as const
+export const directions = ["N", "NW", "W", "SW", "S", "SE", "E", "NE"] as const
 export type Direction = (typeof directions)[number];
 
 export function move(p: Point, d: Direction): Point {
   switch (d) {
-    case "N": return {x: p.x, y: p.y - 1};
-    case "NW": return {x: p.x - 1, y: p.y - 1};
-    case "W": return {x: p.x - 1, y: p.y};
-    case "SW": return {x: p.x - 1, y: p.y + 1};
-    case "S": return {x: p.x, y: p.y + 1};
-    case "SE": return {x: p.x + 1, y: p.y + 1};
-    case "E": return {x: p.x + 1, y: p.y};
-    case "NE": return {x: p.x + 1, y: p.y - 1};
+    case "N": return { x: p.x, y: p.y - 1 };
+    case "NW": return { x: p.x - 1, y: p.y - 1 };
+    case "W": return { x: p.x - 1, y: p.y };
+    case "SW": return { x: p.x - 1, y: p.y + 1 };
+    case "S": return { x: p.x, y: p.y + 1 };
+    case "SE": return { x: p.x + 1, y: p.y + 1 };
+    case "E": return { x: p.x + 1, y: p.y };
+    case "NE": return { x: p.x + 1, y: p.y - 1 };
   }
 }
 
